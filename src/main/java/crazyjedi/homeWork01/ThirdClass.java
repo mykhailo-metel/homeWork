78e4eddf5cd70684645ce9680b02c6d7971ca41f
Vlad <vlad.pythoner@gmail.com>
2017-02-05 10:38:32 +0200

    Добавил ДЗ1

---
+++ src/main/java/crazyjedi/homeWork01/ThirdClass.java
@@ -0,0 +1,10 @@
+package crazyjedi.homeWork01;
+
+/**
+ * Created by Vlad on 05.02.2017.
+ */
+public class ThirdClass {
+    public void PrintMessage(){
+        System.out.println("Мой первый класс на Java");
+    }
+}
