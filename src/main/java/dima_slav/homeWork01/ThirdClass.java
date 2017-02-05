5a96d0a4a0bb612493d82b12c074680f52711432
dima-slav <dzirtdourden4@gmail.com>
2017-02-05 16:40:29 +0200

    Add files via upload

---
+++ src/main/java/dima_slav/homeWork01/ThirdClass.java
@@ -0,0 +1,11 @@
+package Module1;
+
+/**
+ * Created by Dima on 26.01.2017.
+ */
+public class ThirdClass {
+    public static void main(String[] args) {
+
+        System.out.println("Мой прервый класс на Java");
+    }
+}
