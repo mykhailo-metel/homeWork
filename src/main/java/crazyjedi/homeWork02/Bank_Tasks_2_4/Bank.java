2706aa64f4065990cead4fd5cf72080e3a8dd742
Vlad <vlad.pythoner@gmail.com>
2017-02-06 06:37:24 +0200

    Поправил форматирование

--- src/main/java/crazyjedi/homeWork02/Bank_Tasks_2_4/Bank.java
+++ src/main/java/crazyjedi/homeWork02/Bank_Tasks_2_4/Bank.java
@@ -7,7 +7,7 @@ public class Bank {
 
     //Homework 2. Task 2.
 
-    /**
+    /*
      * Write method which withdraw money from account and takes commision 5% of the transaction. Print OK + commision +
      * balance after withdrawal. Print NO is withdrawal is not possible.
      */
@@ -16,7 +16,8 @@ public class Bank {
         double COMISSION = 0.05D;
         double totalWithdrawal = withdrawal * COMISSION + withdrawal;
         if (balance >= totalWithdrawal) {
-            System.out.println(String.format("OK\t%.2f\t%.2f", withdrawal * COMISSION, balance - totalWithdrawal));
+            System.out.println(String.format("OK\t%.2f\t%.2f", withdrawal
+                    * COMISSION, balance - totalWithdrawal));
         } else {
             System.out.println("NO");
         }
@@ -24,23 +25,23 @@ public class Bank {
 
     //Homework 2. Task 3.
 
-    /**
+    /*
      * You need to write method which withdraw money of particular account owner if he/she can. Print name + NO of
      * withdrawal fs not possible and name + sum of withdrawal + balance after withdrawal in other case.
      * Commision is 5% for all cases.
      */
-    public static void withdrawFromOneOfArray(int[] balances, String[] ownerNames, String ownerName, double withdrawal) {
-
+    public static void withdrawFromOneOfArray(int[] balances, String[] ownerNames
+                                                , String ownerName, double withdrawal) {
         //Я предполагаю, что ownerNames содержит уникальные идентификаторы счета
 
-        //Надо удостовериться, что массивы одной длины
-        if (balances.length != ownerNames.length) {
+
+        if (balances.length != ownerNames.length) { /*Надо удостовериться, что массивы одной длины*/
             throw new IllegalArgumentException("Arrays are of different length.");
         }
 
         //Надо найти клиента и баланс
         for (int i = 0; i < balances.length; i++) {
-            if ( ownerNames[i].equals(ownerName)) { //Если клиент найден,
+            if (ownerNames[i].equals(ownerName)) { //Если клиент найден,
                 int balance = balances[i]; //получаем баланс
                 double totalWithdrawal = withdrawal * 1.05; //считаем сумму списания
                 if (totalWithdrawal <= balance) {// проверяем достаточно ли средств на счете
@@ -58,24 +59,23 @@ public class Bank {
 
     //Homework 2. Task 4.
 
-    /**
+    /*
      *You need to write method which will fund (пополнение) balance of particular user. Print name
      * + balance after funding
      */
-    public static void fundOneOfArray(int[] balances, String[] ownerNames, String ownerName, double funding){
+    public static void fundOneOfArray(int[] balances, String[] ownerNames, String ownerName, double funding) {
 
         //Я предполагаю, что ownerNames содержит уникальные идентификаторы счета
-
-        //Надо удостовериться, что массивы одной длины
-        if (balances.length != ownerNames.length) {
+        
+        if (balances.length != ownerNames.length) { /*Надо удостовериться, что массивы одной длины*/
             throw new IllegalArgumentException("Arrays are of different length.");
         }
 
         //Надо найти клиента и баланс
         for (int i = 0; i < balances.length; i++) {
-            if ( ownerNames[i].equals(ownerName)) { //Если клиент найден,
+            if (ownerNames[i].equals(ownerName)) { //Если клиент найден,
                 int balance = balances[i]; //получаем баланс
-                System.out.println(String.format("%s\t%.2f", ownerName,balance+funding));
+                System.out.println(String.format("%s\t%.2f", ownerName, balance + funding));
                 return;
             }
         }
