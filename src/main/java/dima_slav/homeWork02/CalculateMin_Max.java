09839b2bd692527ba65e9f88721981738090e282
dima-slav <dzirtdourden4@gmail.com>
2017-02-07 20:10:41 +0200

    Update CalculateMin_Max.java

--- src/main/java/dima_slav/homeWork02/CalculateMin_Max.java
+++ src/main/java/dima_slav/homeWork02/CalculateMin_Max.java
@@ -1,6 +1,6 @@
 package Task2_1;
 
-public class CalculateMin_Max{
+public class CalculateMinMax{
     public static void main(String[] args) {
         int arrayI[] = {10, 22, 24, 66, -10, -22, -14, 33, 26, 10};
         double arrayD[] = {10.5, 22.5, 24.5, 66.5, -10.5 -22.5, -14.5, 33.5, 26.5, 10.5};
