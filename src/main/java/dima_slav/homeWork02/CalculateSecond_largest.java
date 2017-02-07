9af9390b8c63d372a1d4499b55dab6475b250900
dima-slav <dzirtdourden4@gmail.com>
2017-02-07 20:13:36 +0200

    Update CalculateSecond_largest.java

--- src/main/java/dima_slav/homeWork02/CalculateSecond_largest.java
+++ src/main/java/dima_slav/homeWork02/CalculateSecond_largest.java
@@ -1,7 +1,7 @@
 import static com.sun.javafx.util.Utils.sum;
 
 
-public class CalculateSecond_largest {
+public class CalculateSecondLargest {
     public static void main(String[] args) {
         int arrayI[] = {10, 22, 24, 66, -10, -22, -14, 33, 26, 10};
         double arrayD[] = {10.5, 22.5, 24.5, 66.5, -10.5, -22.5, -14, 5, 33.5, 26.5, 10.5,};
