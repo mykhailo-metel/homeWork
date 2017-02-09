ed9b2b6ccfa38b5fcf5d777a38255ede03fbb088
Mykhailo Metel <ukraine.blizzard@gmail.com>
2017-02-09 17:41:44 +0200

    Начало задания 3

---
+++ src/main/java/mihail_metel/homeWork03/homeWork033/CollegeStudent.java
@@ -0,0 +1,7 @@
+package homeWork03.homeWork033;
+
+/**
+ * Created by fluent on 09.02.2017.
+ */
+public class CollegeStudent {
+}
---
+++ src/main/java/mihail_metel/homeWork03/homeWork033/Solution.java
@@ -0,0 +1,10 @@
+package homeWork03.homeWork033;
+
+/**
+ * Created by fluent on 09.02.2017.
+ */
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+}
