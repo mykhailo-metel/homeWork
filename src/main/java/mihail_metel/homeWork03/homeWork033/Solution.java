9cc890db791c746154bf72d73fd427b28f4ab745
Mykhailo Metel <ukraine.blizzard@gmail.com>
2017-02-14 09:53:53 +0200

    033

--- src/main/java/mihail_metel/homeWork03/homeWork033/Solution.java
+++ src/main/java/mihail_metel/homeWork03/homeWork033/Solution.java
@@ -1,5 +1,6 @@
 package homeWork03.homeWork033;
 
+
 /*You need to create following structure of the classes.
 
         Class Course with fields: Date startDate, String name, int hoursDuration, String teacherName.
@@ -19,8 +20,36 @@ package homeWork03.homeWork033;
         You must have 13 objects in Solution class.
 */
 
+
+import java.util.Date;
+
 public class Solution {
     public static void main(String[] args) {
+        Date someDate1 = new Date(2017,1,1);
+        Date someDate2 = new Date(2017,6,1);
 
+        System.out.println(someDate1);
+
+        Course courseMath = new Course(someDate1, "Math");
+        Course courseJava = new Course("Java", 6, "Games Gosling");
+        Course courseTesting = new Course("Testing", 3, "SomeName");
+        Course courseEnglish = new Course(someDate2, "English");
+        Course courseHybernate = new Course("Hybernate", 6,"Somename2");
+
+        Student studentMisha = new Student("Misha", "Metel", 10);
+        Student studentPetia = new Student("Pupkin", new Course[]{courseJava, courseTesting});
+
+        CollegeStudent john = new CollegeStudent("John", "Smith",5);
+        CollegeStudent ania = new CollegeStudent("Ivanova", new Course[]{courseMath, courseEnglish});
+        CollegeStudent petia = new CollegeStudent("Petia", "Petkin",
+                5, "KPI", 15, 100 );
+
+        SpecialStudent katia = new SpecialStudent("Katia", "Pupkina", 2);
+        SpecialStudent serega = new SpecialStudent("Zverev", new Course[]{courseJava, courseMath});
+        SpecialStudent kolian = new SpecialStudent("Kolian", "Kolianov",3,321654);
     }
+
+
+
+
 }
