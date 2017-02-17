17a8eb2549aeccb84d053928ff628ec629a6711c
dima-slav <dzirtdourden4@gmail.com>
2017-02-17 18:18:55 +0200

    Add files via upload

---
+++ src/main/java/dima_slav/homework03/GoJava#6_Module3_Task3.1/Task3_1/Bird.java
@@ -0,0 +1,8 @@
+package Task3_1;
+
+public class Bird {
+    public void sing(String words){
+        System.out.println(words);
+    }
+}
+
---
+++ src/main/java/dima_slav/homework03/GoJava#6_Module3_Task3_2/Task3_2/Arithmetic.java
@@ -0,0 +1,10 @@
+package Task3_2;
+
+
+public class Arithmetic {
+
+    public static int add(Integer a, Integer b){
+        int sum = a + b;
+        return  sum;
+    }
+}
