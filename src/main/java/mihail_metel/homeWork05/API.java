f69ca027c6c6f18594424e11c3ea379c94aefbd7
Mykhailo Metel <ukraine.blizzard@gmail.com>
2017-02-27 18:58:09 +0200

    05

--- src/main/java/mihail_metel/homeWork05/API.java
+++ src/main/java/mihail_metel/homeWork05/API.java
@@ -5,6 +5,6 @@ TASK  3
 Create API interface which contains one method
 Room[] findRooms(int price, int persons, String city, String hotel)
  */
-public interface API_findRooms {
+public interface API {
     Room[] findRooms(int price, int persons, String city, String hotel);
 }
---
+++ src/main/java/mihail_metel/homeWork05/HotelsDAOImpl.java
@@ -0,0 +1,27 @@
+package homeWork05;
+
+/**
+ * Created by fluent on 27.02.2017.
+ */
+public class HotelsDAOImpl implements HotelsDAO {
+
+    @Override
+    public Room save(Room room) {
+        return null;
+    }
+
+    @Override
+    public boolean delete(Room room) {
+        return false;
+    }
+
+    @Override
+    public Room update(Room room) {
+        return null;
+    }
+
+    @Override
+    public Room findById(long id) {
+        return null;
+    }
+}
